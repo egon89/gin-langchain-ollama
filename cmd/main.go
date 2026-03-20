38e6a7474d5c13963cc6bce48d21fba4cdf4c64a
Everton Goncalves <everton.goncalves.89@gmail.com>
2026-03-20 00:26:14 -0300

    feat: chat memory

--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,25 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"regexp"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/ollama"
+	"github.com/tmc/langchaingo/memory"
 )
 
 // instance of ollama LLM
 var llm = ollamaFactory()
+
+var chatMemories = make(map[string]*memory.ConversationBuffer)
+var mu sync.Mutex
 
 func main() {
 	router := gin.Default()
@@ -22,12 +28,6 @@ func main() {
 	router.Static("/public", "./public")
 
 	router.Use(ErrorHandler())
-
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
 
 	router.GET("/chat", func(c *gin.Context) {
 		message := c.DefaultQuery("message", "Give me a short poem")
@@ -49,54 +49,64 @@ func main() {
 			message = "Give me a short poem"
 		}
 
+		// Get conversation history
+		chatMemory := getChatMemory(chatId)
+		chatMessages, _ := chatMemory.ChatHistory.Messages(ctx)
+
+		messageContentList := createMessageContentList(chatMessages, message)
+
 		log.Printf("Received message for chat %s: %s", chatId, message)
 
 		var lastChunk string
 		var punctuationRegex = regexp.MustCompile(`^[.,!?)]`)
 
-		_, err := llms.GenerateFromSinglePrompt(
-			ctx,
-			llm,
-			message,
-			llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
-				if isDisconnected(ctx) {
-					log.Println("client disconnected, stopping stream")
-					return ctx.Err()
-				}
+		resp, err := llm.GenerateContent(ctx, messageContentList, llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+			if isDisconnected(ctx) {
+				log.Println("client disconnected, stopping stream")
+				return ctx.Err()
+			}
 
-				current := string(chunk)
+			current := string(chunk)
 
-				// First chunk → just send
-				if lastChunk == "" {
-					lastChunk = current
-					c.SSEvent("message", current)
-					c.Writer.Flush()
-					return nil
-				}
-
-				// Check if we need a space
-				needsSpace := !strings.HasSuffix(lastChunk, " ") &&
-					!punctuationRegex.MatchString(current)
-
-				output := current
-				if needsSpace {
-					output = " " + current
-				}
-
+			// First chunk → just send
+			if lastChunk == "" {
 				lastChunk = current
-
-				c.SSEvent("message", output)
+				c.SSEvent("message", current)
 				c.Writer.Flush()
-
 				return nil
-			}),
+			}
+
+			// Check if we need a space
+			needsSpace := !strings.HasSuffix(lastChunk, " ") &&
+				!punctuationRegex.MatchString(current)
+
+			output := current
+			if needsSpace {
+				output = " " + current
+			}
+
+			lastChunk = current
+
+			c.SSEvent("message", output)
+			c.Writer.Flush()
+
+			return nil
+		}),
 		)
 		if err != nil {
 			log.Fatal(err)
 		}
 
+		finalResponse := getResponse(resp, c)
+
 		c.SSEvent("message", "<END_OF_RESPONSE>")
 		c.Writer.Flush()
+
+		fmt.Printf("Final response for chat %s: %s\n", chatId, finalResponse)
+
+		// Save the conversation to memory
+		chatMemory.ChatHistory.AddUserMessage(ctx, message)
+		chatMemory.ChatHistory.AddAIMessage(ctx, finalResponse)
 	})
 
 	router.GET("/api/stream", func(c *gin.Context) {
@@ -153,4 +163,55 @@ func ollamaFactory() *ollama.LLM {
 	log.Println("Ollama LLM initialized with model")
 
 	return llm
+}
+
+func getChatMemory(chatId string) *memory.ConversationBuffer {
+	mu.Lock()
+	defer mu.Unlock()
+
+	log.Printf("Retrieving memory for chat ID: %s", chatId)
+
+	mem, exists := chatMemories[chatId]
+	if !exists {
+		mem = memory.NewConversationBuffer()
+		chatMemories[chatId] = mem
+	}
+
+	return mem
+}
+
+func getResponse(response *llms.ContentResponse, c *gin.Context) string {
+	choices := response.Choices
+
+	if len(choices) < 1 {
+		c.String(http.StatusOK, "no response from model")
+	}
+
+	return choices[0].Content
+}
+
+func createMessageContentList(chatMessages []llms.ChatMessage, lastMessage string) []llms.MessageContent {
+	var messageContentList []llms.MessageContent
+
+	for _, msg := range chatMessages {
+		switch msg.GetType() {
+		case "human":
+			messageContentList = append(messageContentList, llms.MessageContent{
+				Role:  llms.ChatMessageTypeHuman,
+				Parts: []llms.ContentPart{llms.TextContent{Text: msg.GetContent()}},
+			})
+		case "ai":
+			messageContentList = append(messageContentList, llms.MessageContent{
+				Role:  llms.ChatMessageTypeAI,
+				Parts: []llms.ContentPart{llms.TextContent{Text: msg.GetContent()}},
+			})
+		}
+	}
+
+	messageContentList = append(messageContentList, llms.MessageContent{
+		Role:  llms.ChatMessageTypeHuman,
+		Parts: []llms.ContentPart{llms.TextContent{Text: lastMessage}},
+	})
+
+	return messageContentList
 }
